6923eeed3cb631e2f764231aa91a7e1ee7fc783e
Muhammad Zahid <muhammadali775@gmail.com>
2021-04-29 02:14:03 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(array, string){
+
+
+ let newArray = array.filter((names) => {return names.toUpperCase() === string.toUpperCase()})
+
+ return newArray;
+}
+
+function fuzzyMatch(array, string) {
+
+ let newArray = array.filter((names) => {return names[0] === string[0]})                                                 
+
+return newArray
+}
+
+function matchName(array, string) {
+
+
+let newArray = array.filter((names) => {return names.name === string})
+
+    return newArray
+}
